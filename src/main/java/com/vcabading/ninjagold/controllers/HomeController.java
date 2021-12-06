5f66a415025391e94d9c71f39c77721520f0880c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-05 22:18:49 -0800

    add dependencies

--- src/main/java/com/vcabading/ninjagold/controllers/HomeController.java
+++ src/main/java/com/vcabading/ninjagold/controllers/HomeController.java
@@ -1,5 +1,18 @@
 package com.vcabading.ninjagold.controllers;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+/////////////////////////////////////////////////////////////////
+//HOME CONTROLLER
+/////////////////////////////////////////////////////////////////
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("dojoName", "San Jose");
+		return "index.jsp";
+	}
 }
